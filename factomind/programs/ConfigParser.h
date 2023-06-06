5c34e73de4349af5bd132f300dc75bfbe8e79e49
akhil <akhiltiwari.13@gmail.com>
2023-06-06 14:35:37 +0530

    [factomind] remove factomind dir from the project (temporarily) to remove the clutter in this repo

--- factomind/programs/ConfigParser.h
+++
@@ -1,9 +0,0 @@
-#ifndef CONFIGPARSER_H
-#define CONFIGPARSER_H
-
-#include <map>
-#include <string>
-
-std::map<std::string, std::map<std::string, double>> parseConfig(const std::string& filename);
-
-#endif  // CONFIGPARSER_H
